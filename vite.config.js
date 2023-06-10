2d92e79ee13dd4049fb0ecc38777eb8ebea1aacd
rachellarralde <132167926+witchaudio@users.noreply.github.com>
2023-06-10 13:58:04 -0700

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist', 
-  },
+  root: './src',
+  main: 'main.jsx', 
 })
